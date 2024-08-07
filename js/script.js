96b74063ee6c935b8f1c79cd2325904244836c29
Zosin0 <lucaszoser1@gmail.com>
2024-08-07 17:39:54 -0300

    other files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.oficina-item').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.transform = 'scale(1.05)';
+        item.style.transition = 'transform 0.2s';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.transform = 'scale(1)';
+    });
+});
